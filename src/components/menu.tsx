a5fc6f55206e5b868dc7d3af266238a925dfbdd7
git config --global user.name acb2769 <git config --global user.email acb2769@gmail.com>
2026-02-23 20:22:56 -0300

    Alteração Nav

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -11,13 +11,13 @@ export function Menu() {
     const selectedCss = 'py-2 px-4 rounded-xl bg-amber-500 text-zinc-50'
     return (
         <nav className="bg-cyan-900 h-16 flex items-center justify-between px-8 text-zinc-100">
-            <h1 className="font-serif tracking-tight">Sistema<span className="text-amber-500">.</span></h1>
+            <h1 className="font-serif tracking-tight">Controller<span className="text-amber-500">.</span></h1>
             <ul className="flex gap-6 font-sans">
-                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl "><NavLink to="/" className={`${routeSelected === '/' ? selectedCss : ''}`}>Início</NavLink></li>
-                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/sobre" className={`${routeSelected === '/sobre' ? selectedCss : ''}`}>Sobre</NavLink></li>
-                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/servicos" className={`${routeSelected === '/servicos' ? selectedCss : ''}`}>Serviços</NavLink></li>
-                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/portfolio" className={`${routeSelected === '/portfolio' ? selectedCss : ''}`}>Portfólio</NavLink></li>
-                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/contato" className={`${routeSelected === '/contato' ? selectedCss : ''}`}>Contato</NavLink></li>
+                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl "><NavLink to="/" className={`${routeSelected === '/' ? selectedCss : ''}`}>Home</NavLink></li>
+                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/sobre" className={`${routeSelected === '/sobre' ? selectedCss : ''}`}>taskMaster</NavLink></li>
+                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/servicos" className={`${routeSelected === '/servicos' ? selectedCss : ''}`}>connectHub</NavLink></li>
+                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/portfolio" className={`${routeSelected === '/portfolio' ? selectedCss : ''}`}>moneyFlow</NavLink></li>
+                <li className=" hover:text-zinc-50 text-zinc-400 rounded-xl"><NavLink to="/contato" className={`${routeSelected === '/contato' ? selectedCss : ''}`}>Exit</NavLink></li>
             </ul>
         </nav>
     )
