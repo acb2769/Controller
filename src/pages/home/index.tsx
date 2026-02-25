9aa76f159d8c1340860ab085ba9f2730d6ed7160
git config --global user.name acb2769 <git config --global user.email acb2769@gmail.com>
2026-02-25 02:21:07 -0300

    modulos

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,8 +4,27 @@ export function Home() {
 
     return (
         <>
-            <h1>Exemplo de página inicial</h1>
-            <NavLink className='text-xs text-cyan-800 underline' to={'/contato'}>Link para página de contato</NavLink>
+         <div className="max-w-md mx-auto p-10 bg-white shadow-lg rounded-xl mt-20">
+            <h1 className="font-serif text-2xl tracking-tight font-bold mb-6 text-center">Bem vindos ao Controller<span className="text-amber-500">.</span>
+            </h1>
+            <div className="border-b border-gray-200 my-6"></div>
+            <h2 className="font-sans text-lg font-bold mb-4">Um ecossistema para ajudar no controle de seus projetos pessoais.</h2>
+                <h3 className="font-sans text-lg font-bold mb-2">Módulo moneyFlow:</h3>
+                <div className="flex flex-col">   
+                    <p className="font-sans text-md font-medium mb-4 border border-gray-200 shadow-p p-2 rounded">
+                        Para o controle de suas finanças</p>
+                </div>
+                <h3 className="font-sans text-lg font-bold mb-2">Módulo connectHub:</h3>
+                <div className="flex flex-col">   
+                    <p className="font-sans text-md font-medium mb-4 border border-gray-200 shadow-p p-2 rounded">
+                        Para o cadastro de seus contatos e conexões</p>
+                </div>
+                <h3 className="font-sans text-lg font-bold mb-2">Módulo taskMaster:</h3>
+                <div className="flex flex-col">   
+                    <p className="font-sans text-md font-medium mb-4 border border-gray-200 shadow-p p-2 rounded">
+                        Para o cadastro e controle de suas tarefas diárias</p>
+                </div>
+        </div>
         </>
     )
 }
